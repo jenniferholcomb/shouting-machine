00cbe5057c7378bc3c5bde73e3ff4d692c5ab169
Jennifer Holcomb <jenniferlholcomb@gmail.com>
2022-10-19 11:29:42 -0700

    Add run function.
    Co-authored-by: vw <vera.weikel@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -5,18 +5,28 @@
 
 //get input from user
 function getAndSetShoutingMachineValues() { // aka getandSetMadLibsVal()
-  // intake from user
+  // intake from user GET
   const shoutingInput = document.getElementById("user-input").value;
-  // display back to user
-
+  // display back to user SET
+  document.querySelector("span#shout").innerText = shoutingInput;
+  console.log("const shoutingInput: ", shoutingInput);
 }
 
-
-form.onsubmit = function(event) {
-  document.querySelector("form");
+// this function creates the onsubmit event handler
+// it needs to be called in the window.onload event handler
+// the final display fx()
+function setFormSubmissionEventHandler(){
+ 
+  let form = document.querySelector("form");
+  form.onsubmit = function(event) {
+    event.preventDefault();
+    getAndSetShoutingMachineValues();
+    //write to the div - 
+    document.querySelector("div#shout-display-response");
+  }
 }
 
 window.onload = function(){
-  event.preventDefault();
-// the final display fx()
+  //runs the display fx()
+  setFormSubmissionEventHandler();
 }
