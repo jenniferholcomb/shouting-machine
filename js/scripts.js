881104c6fd5c439e4e57572574cb0f87f934b4e5
Jennifer Holcomb <jenniferlholcomb@gmail.com>
2022-10-19 10:37:46 -0700

    Add user-input id to html and scripts.
    Co-authored-by: vw <vera.weikel@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+// Business logic
+
+
+// UI logic
+
+//get input from user
+function getAndSetMadLibValues() {
+  const shoutingInput = document.getElementById("user-input").value;
+
+}
+
+
+form.onsubmit = function(event) {
+  document.querySelector("form");
+}
+
+window.onload = function(){
+  event.preventDefault();
+
+}
